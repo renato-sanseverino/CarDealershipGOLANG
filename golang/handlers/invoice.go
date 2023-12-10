96be6b658cd49bd642b56a237cd6acac71566bd0
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-12-10 18:41:36 -0300

    Inlcuido handler para o invoice

--- golang/handlers/invoice.go
+++ golang/handlers/invoice.go
@@ -1,8 +1,11 @@
-package handlers;
+package handlers
 
 import (
-	"net/http"
+	"cardealership/prisma/db"
 	"cardealership/utils"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,6 +23,81 @@ func GetInvoices(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": invoices})
 }
 
-// func PostInvoice(c *gin.Context) {
-// ...
-// ...
+func PostInvoice(c *gin.Context) {
+	var payload db.InnerInvoice
+
+	// Bind JSON body to the struct
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	client := utils.GetPrisma(c)
+	insertedInvoice, err := client.Invoice.CreateOne(
+		db.Invoice.Amount.Set(payload.Amount),
+		db.Invoice.CustomerID.SetIfPresent(&payload.CustomerID),
+		db.Invoice.SalespersonID.SetIfPresent(&payload.SalespersonID),
+		db.Invoice.CarID.SetIfPresent(&payload.CarID),
+	).Exec(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Invoice created successfully", "invoice": insertedInvoice})
+}
+
+func PatchInvoice(c *gin.Context) {
+	var payload db.InnerInvoice
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	// Bind JSON body to the struct
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	client := utils.GetPrisma(c)
+	updatedInvoice, err := client.Invoice.FindUnique(
+		db.Invoice.ID.Equals(id),
+	).Update(
+		db.Invoice.Amount.Set(payload.Amount),
+		db.Invoice.CustomerID.SetIfPresent(&payload.CustomerID),
+		db.Invoice.SalespersonID.SetIfPresent(&payload.SalespersonID),
+		db.Invoice.CarID.SetIfPresent(&payload.CarID),
+	).Exec(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Invoice patched", "invoice": updatedInvoice})
+}
+
+func DeleteInvoice(c *gin.Context) {
+	// TODO: utilizar o flag_removed ao invés de apagar o registro na tabela
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	client := utils.GetPrisma(c)
+	deletedInvoice, err := client.Invoice.FindUnique(
+		db.Invoice.ID.Equals(id),
+	).Delete().Exec(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Invoice deleted successfully", "id": deletedInvoice.ID})
+}
